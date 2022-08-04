cfb13dd9d3e106d7b4a0eaac41925d5690a4ee33
vivekmv99 <mvvivek99@gmail.com>
2022-08-04 19:46:43 +0530

    creating models and template

--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+
+class Products(models.Model):
+    category = models.ForeignKey(Category, related_name="category_field", on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100,null=True)
+    price = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='media/products', null=True)
+
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from .models import *
+
+
+def index(request):
+    category = Category.objects.all()
+    context = {'category': category}
+    return render(request,'category_table.html',context)
